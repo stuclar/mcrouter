dcddca0cfda917d26700fc919481460288457817
stuclar <stuclar@devbig485.prn2.facebook.com>
2020-03-26 23:39:02 -0700

    Next bug
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- mcrouter/StandaloneConfig.cpp
+++ mcrouter/StandaloneConfig.cpp
@@ -41,6 +41,12 @@ getThriftConnectionAclChecker(
     bool /* enforce */) {
   return [](McServerSession&) { return true; };
 }
+std::function<bool(const folly::AsyncTransportWrapper*)>
+getThriftConnectionAclChecker(
+    const std::string& /* serviceIdentity */,
+    bool /* enforce */) {
+  return [](const folly::AsyncTransportWrapper*) { return true; };
+}
 
 } // namespace mcrouter
 } // namespace memcache
